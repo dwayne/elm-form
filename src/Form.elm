9fdd058c5f4b2385d5ae2f3ba5ac2529b89ec074
Dwayne Crooks <me@dwaynecrooks.com>
2025-08-19 05:50:20 -0400

    Document Form

--- src/Form.elm
+++ src/Form.elm
@@ -1,15 +1,13 @@
 module Form exposing
     ( Form
     , new
-    , Accessor
-    , get
-    , set, modify, update
+    , Accessor, get, set, modify, update
     , isValid, isInvalid
     , validate, validateAsMaybe, validateAsResult
     , toState
     )
 
-{-|
+{-| It provides a way to package the fields, accessors, and business logic pertaining to a form behind a consistent interface.
 
 
 # Form
@@ -24,9 +22,7 @@ module Form exposing
 
 # Accessors
 
-@docs Accessor
-@docs get
-@docs set, modify, update
+@docs Accessor, get, set, modify, update
 
 
 # Query
@@ -48,7 +44,14 @@ module Form exposing
 import Validation as V exposing (Validation)
 
 
-{-| -}
+{-|
+
+  - `state`: Typically a record containing fields and possibly other forms.
+  - `accessors`: Typically a record containing getters/setters and anything else that needs access to the state of the form.
+  - `error`: The errors produced by an invalid form.
+  - `output`: The data produced by a valid form.
+
+-}
 type Form state accessors error output
     = Form
         { state : state
@@ -80,32 +83,55 @@ new options =
 -- ACCESSORS
 
 
-{-| -}
+{-| Combines a way to `get` a value from a property on `state` and a way to `modify` that property's value.
+
+    type alias State =
+        { count : Int
+        , name : String
+        }
+
+    countAccessor : Accessor State Int
+    countAccessor =
+        { get = .count
+        , modify = \f state -> { state | count = f state.count }
+        }
+
+    nameAccessor : Accessor State String
+    nameAccessor =
+        { get = .name
+        , modify = \f state -> { state | name = f state.name }
+        }
+
+-}
 type alias Accessor state a =
     { get : state -> a
     , modify : (a -> a) -> state -> state
     }
 
 
-{-| -}
+{-| Get the value of a property on the given form.
+-}
 get : (accessors -> Accessor state a) -> Form state accessors error output -> a
 get toAccessor (Form form) =
     (toAccessor form.accessors).get form.state
 
 
-{-| -}
+{-| Set the value of a property on the given form.
+-}
 set : (accessors -> Accessor state a) -> a -> Form state accessors error output -> Form state accessors error output
 set toAccessor x =
     modify toAccessor (always x)
 
 
-{-| -}
+{-| Modify the value of a property on the given form.
+-}
 modify : (accessors -> Accessor state a) -> (a -> a) -> Form state accessors error output -> Form state accessors error output
 modify toAccessor t (Form form) =
     Form { form | state = (toAccessor form.accessors).modify t form.state }
 
 
-{-| -}
+{-| Update the value of a property on the given form.
+-}
 update : (accessors -> state -> state) -> Form state accessors error output -> Form state accessors error output
 update f (Form form) =
     Form { form | state = f form.accessors form.state }
