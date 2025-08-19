ae71df78f96557f527baca9d94de0c588246049b
Dwayne Crooks <me@dwaynecrooks.com>
2025-08-18 20:37:54 -0400

    Start to add docs

--- src/Form.elm
+++ src/Form.elm
@@ -1,23 +1,54 @@
 module Form exposing
-    ( Accessor
-    , Form
-    , Options
-    , get
-    , isInvalid
-    , isValid
-    , modify
+    ( Form
     , new
-    , set
+    , Accessor
+    , get
+    , set, modify, update
+    , isValid, isInvalid
+    , validate, validateAsMaybe, validateAsResult
     , toState
-    , update
-    , validate
-    , validateAsMaybe
-    , validateAsResult
     )
+
+{-|
+
+
+# Form
+
+@docs Form
+
+
+# Construct
+
+@docs new
+
+
+# Accessors
+
+@docs Accessor
+@docs get
+@docs set, modify, update
+
+
+# Query
+
+@docs isValid, isInvalid
+
+
+# Validate
+
+@docs validate, validateAsMaybe, validateAsResult
+
+
+# Convert
+
+@docs toState
+
+-}
 
 import Validation as V exposing (Validation)
 
 
+{-| -}
 type Form state accessors error output
     = Form
         { state : state
@@ -30,14 +61,13 @@ type Form state accessors error output
 -- CONSTRUCT
 
 
-type alias Options state accessors error output =
+{-| -}
+new :
     { init : state
     , accessors : accessors
     , validate : state -> Validation error output
     }
-
-
-new : Options state accessors error output -> Form state accessors error output
+    -> Form state accessors error output
 new options =
     Form
         { state = options.init
@@ -47,38 +77,35 @@ new options =
 
 
 
--- ACCESSOR
+-- ACCESSORS
 
 
-type alias Accessor s a =
-    { get : s -> a
-    , modify : (a -> a) -> s -> s
+{-| -}
+type alias Accessor state a =
+    { get : state -> a
+    , modify : (a -> a) -> state -> state
     }
 
 
-
--- GET
-
-
+{-| -}
 get : (accessors -> Accessor state a) -> Form state accessors error output -> a
 get toAccessor (Form form) =
     (toAccessor form.accessors).get form.state
 
 
-
--- MODIFY
-
-
-modify : (accessors -> Accessor state a) -> (a -> a) -> Form state accessors error output -> Form state accessors error output
-modify toAccessor t (Form form) =
-    Form { form | state = (toAccessor form.accessors).modify t form.state }
-
-
+{-| -}
 set : (accessors -> Accessor state a) -> a -> Form state accessors error output -> Form state accessors error output
 set toAccessor x =
     modify toAccessor (always x)
 
 
+{-| -}
+modify : (accessors -> Accessor state a) -> (a -> a) -> Form state accessors error output -> Form state accessors error output
+modify toAccessor t (Form form) =
+    Form { form | state = (toAccessor form.accessors).modify t form.state }
+
+
+{-| -}
 update : (accessors -> state -> state) -> Form state accessors error output -> Form state accessors error output
 update f (Form form) =
     Form { form | state = f form.accessors form.state }
@@ -88,11 +115,15 @@ update f (Form form) =
 -- QUERY
 
 
+{-| `True` if the form does not have any errors.
+-}
 isValid : Form state accessors error output -> Bool
 isValid =
     validate >> V.isValid
 
 
+{-| `True` if the form has errors. `isInvalid form` is equivalent to `not (isValid form)`.
+-}
 isInvalid : Form state accessors error output -> Bool
 isInvalid =
     not << isValid
@@ -102,16 +133,19 @@ isInvalid =
 -- VALIDATE
 
 
+{-| -}
 validate : Form state accessors error output -> Validation error output
 validate (Form form) =
     form.validate form.state
 
 
+{-| -}
 validateAsResult : Form state accessors error output -> Result (List error) output
 validateAsResult =
     validate >> V.toResult
 
 
+{-| -}
 validateAsMaybe : Form state accessors error output -> Maybe output
 validateAsMaybe =
     validate >> V.toMaybe
@@ -121,6 +155,7 @@ validateAsMaybe =
 -- CONVERT
 
 
+{-| -}
 toState : Form state accessors error output -> state
 toState (Form { state }) =
     state
--- src/Form/List.elm
+++ src/Form/List.elm
@@ -1,20 +1,55 @@
 module Form.List exposing
     ( Forms
-    , append
-    , empty
-    , fromList
-    , get
-    , modify
-    , prepend
-    , remove
-    , toList
+    , empty, fromList
+    , get, modify
+    , prepend, append, remove
     , validate
+    , toList
     )
+
+{-|
+
+
+# Forms
+
+@docs Forms
+
+
+# Construct
+
+@docs empty, fromList
+
+
+# Accessors
+
+@docs get, modify
+
+
+# Change
+
+@docs prepend, append, remove
+
+
+# Validate
+
+@docs validate
+
+
+# Convert
+
+@docs toList
+
+-}
 
 import Form exposing (Accessor, Form)
 import Validation as V exposing (Validation)
 
 
+
+-- FORMS
+
+
+{-| -}
 type Forms a
     = Forms
         { id : Int
@@ -22,6 +57,11 @@ type Forms a
         }
 
 
+
+-- CONSTRUCT
+
+
+{-| -}
 empty : Forms a
 empty =
     Forms
@@ -30,6 +70,7 @@ empty =
         }
 
 
+{-| -}
 fromList : List a -> Forms a
 fromList list =
     let
@@ -52,6 +93,7 @@ fromListHelper i elements list =
             fromListHelper (i + 1) (( i, x ) :: elements) xs
 
 
+{-| -}
 get : Int -> (accessors -> Accessor state a) -> a -> Forms (Form state accessors error output) -> a
 get id toAccessor default (Forms { elements }) =
     elements
@@ -61,6 +103,7 @@ get id toAccessor default (Forms { elements }) =
         |> Maybe.withDefault default
 
 
+{-| -}
 modify : Int -> (accessors -> Accessor state a) -> (a -> a) -> Forms (Form state accessors error output) -> Forms (Form state accessors error output)
 modify id toAccessor f (Forms c) =
     Forms
@@ -80,21 +123,33 @@ modify id toAccessor f (Forms c) =
         }
 
 
+
+-- CHANGE
+
+
+{-| -}
 prepend : a -> Forms a -> Forms a
 prepend x (Forms c) =
     Forms { c | id = c.id + 1, elements = ( c.id, x ) :: c.elements }
 
 
+{-| -}
 append : a -> Forms a -> Forms a
 append x (Forms c) =
     Forms { c | id = c.id + 1, elements = c.elements ++ [ ( c.id, x ) ] }
 
 
+{-| -}
 remove : Int -> Forms a -> Forms a
 remove id (Forms c) =
     Forms { c | elements = List.filter (Tuple.first >> (/=) id) c.elements }
 
 
+
+--VALIDATE
+
+
+{-| -}
 validate : (x -> y) -> Forms (Form state accessors x output) -> Validation y (List output)
 validate f (Forms c) =
     List.foldr
@@ -103,6 +158,14 @@ validate f (Forms c) =
         c.elements
 
 
+
+--
+-- What about validateResult and validateMaybe?
+--
+-- CONVERT
+
+
+{-| -}
 toList : Forms a -> List ( Int, a )
 toList (Forms { elements }) =
     elements
